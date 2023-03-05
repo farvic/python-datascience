c2c2fcb1e93d8f08894840a0943477f72ba2af09
Victor <vicfaraujo@gmail.com>
2023-03-05 19:34:41 -0300

    [DS Track] 2 - Intermediate Python, chap3

---
+++ 1 - intro to python/defining_class.py
@@ -0,0 +1,10 @@
+class Planet:
+    def __init__(self, name, diameter_km):
+        self.name = name
+        self.diameter_km = diameter_km
+
+
+e = Planet('Earth', 12742)
+
+
+e.name, e.diameter_km
---
+++ 1 - intro to python/docstring.py
@@ -0,0 +1,6 @@
+def factorial(n):
+    """returns n!"""
+    return 1 if n < 2 else n * factorial(n-1)
+
+
+factorial.__doc__
