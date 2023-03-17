4aff77ca4981414f35cc5394882b226d760f2775
Victor <vicfaraujo@gmail.com>
2023-03-17 04:25:08 -0300

    [DS Track] Complete

---
+++ data-science-track/33_unsupervised_learning_in_python/show_image_function.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+
+def show_as_image(sample):
+    bitmap = sample.reshape((13, 8))
+    plt.figure()
+    plt.imshow(bitmap, cmap='gray', interpolation='nearest')
+    plt.colorbar()
+    plt.show()
